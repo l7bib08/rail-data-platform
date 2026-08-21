5fb2f5c07dd911453eb316a6f4df2591cc0850a7
MOUAD LAHBIB ARBAOUI <mouadlahbibarbaoui08@gmail.com>
2026-08-21 19:40:21 +0100

    Add Parquet

--- src/ingestion.py
+++ src/ingestion.py
@@ -1,6 +1,9 @@
 import requests
 import pandas as pd
 import sys
+from datetime import datetime
+
+
 
 url = sys.argv[1] if len(sys.argv) > 1 else "https://ressources.data.sncf.com/api/explore/v2.1/catalog/datasets/frequentation-gares/records?limit=20"
 
@@ -9,6 +12,9 @@ try:
     if response.status_code == 200:
         data = response.json()
         df = pd.DataFrame(data["results"])
+        date_du_jour = datetime.now().strftime("%Y-%m-%d")
+        df.to_parquet(f"raw/frequentation_{date_du_jour}.parquet", index=False)
+
         print(df.head())
         print(df.columns)
         print(df.shape)
